bcd93f8c1e91b0a2e0ee7d31007e1e90c0f8070e
val <djonsilver@yandex.ru>
2023-12-17 02:06:19 +0300

    db connection for monolit and mcs

--- ads_monolit/db/schema.rb
+++ ads_monolit/db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_163412) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_05_11_163412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -23,16 +22,16 @@ ActiveRecord::Schema.define(version: 2020_05_11_163412) do
     t.float "lat"
     t.float "lon"
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_ads_on_user_id"
   end
 
   create_table "user_sessions", force: :cascade do |t|
     t.uuid "uuid", null: false
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_user_sessions_on_user_id"
     t.index ["uuid"], name: "index_user_sessions_on_uuid"
   end
@@ -41,8 +40,8 @@ ActiveRecord::Schema.define(version: 2020_05_11_163412) do
     t.string "name", null: false
     t.citext "email", null: false
     t.string "password_digest", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
