514a1489ee61a8e95858d43a74afab413fd1d758
val <djonsilver@yandex.ru>
2023-12-17 23:49:33 +0300

    rake plus migrations

--- ads_microservice/config/application_loader.rb
+++ ads_microservice/config/application_loader.rb
@@ -2,11 +2,21 @@ module ApplicationLoader
   extend self
 
   def load_app!
+    init_config
+    init_db
     require_app
     init_app
   end
 
   private
+
+  def init_config
+    require_file 'config/initializers/config'
+  end
+
+  def init_db
+    require_file 'config/initializers/db'
+  end
 
   def require_app
     require_file 'config/application'
